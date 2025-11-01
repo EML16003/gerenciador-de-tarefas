2537fb600c6bcf48bf14e452f4f1817ceab6ebab
Edson Marques de Lima <edsonmarqueslima30@gmail.com>
2025-11-01 18:47:20 -0300

    Add files via upload

---
+++ taskStore.js
@@ -0,0 +1,10 @@
+const TaskStorage = {
+  getTasks: function () {
+    const tasks = localStorage.getItem('tasks');
+    return tasks ? JSON.parse(tasks) : [];
+  },
+
+  saveTasks: function (tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+};
